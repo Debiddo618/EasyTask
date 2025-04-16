6e35f63966908bcd1b4704c6b8a7de8d05ce2799
Debiddo <dzheng@terpmail.umd.edu>
2025-04-16 12:35:00 -0700

    created a user list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [CommonModule, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  onSelectUser(id: string) {
+    // console.log('Selected user ID:', id);
+    this.selectedUserId = id;
+  }
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
 
 @Component({
   selector: 'app-user',
@@ -7,12 +14,29 @@ import { Component, Input } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
+
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+
+
+
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser() {}
+  // Signal based approach
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
