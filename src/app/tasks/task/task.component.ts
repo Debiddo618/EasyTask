ece69cb48ec48ac629e3bf8a9cb68ff814f9ed18
Debiddo <dzheng@terpmail.umd.edu>
2025-04-18 14:15:39 -0700

    add localstorage for data storage

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
 import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 
 
@@ -12,11 +13,13 @@ import { DatePipe } from '@angular/common';
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
+
 export class TaskComponent {
   @Input({required: true}) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  constructor(private tasksService: TasksService) {}
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -31,6 +31,13 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -42,8 +49,14 @@ export class TasksService {
       summary: task.summary,
       dueDate: task.date,
     });
+    this.saveTasks();
   }
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
